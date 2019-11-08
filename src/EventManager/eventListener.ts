c18ada17a613a460f7f12f761075e0a48cd9e07b
Weffe <rogelio_negrete@outlook.com>
2019-11-08 15:20:51 -0800

    improve: fix comment

--- src/EventManager/eventListener.ts
+++ src/EventManager/eventListener.ts
@@ -4,7 +4,7 @@ export function eventListenerFactory(jQuery: JQueryStatic) {
     function addEventListener(type: PageEvent, listener: Function) {
         /**
          * unfortunalety, need to cast to string and any as
-         * we can use Parameters<F> type to pull out the params
+         * we cant use Parameters<F> type to pull out the params
          * for the overloaded .on() function because of this
          * @see https://stackoverflow.com/questions/58773217/how-to-use-parameters-type-on-overloaded-functions
          */
@@ -14,7 +14,7 @@ export function eventListenerFactory(jQuery: JQueryStatic) {
     function removeEventListener(type: PageEvent, listener: Function) {
         /**
          * unfortunalety, need to cast to string and any as
-         * we can use Parameters<F> type to pull out the params
+         * we cant use Parameters<F> type to pull out the params
          * for the overloaded .on() function because of this
          * @see https://stackoverflow.com/questions/58773217/how-to-use-parameters-type-on-overloaded-functions
          */
