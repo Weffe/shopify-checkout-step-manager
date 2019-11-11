2635946ee1ada78dc5d749ca7123eee7831dc042
Weffe <rogelio_negrete@outlook.com>
2019-11-11 13:57:01 -0800

    fix: fix issue with config options for jQuery

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -3,8 +3,8 @@ export interface IOptions {
      * Used to log info during runtime which can be helpful during
      * development or debugging. But should be turned off in production.
      */
-    debug: boolean;
-    jQuery: JQueryStatic;
+    debug?: boolean;
+    jQuery?: JQueryStatic;
 }
 
 export interface IBaseCSM {
